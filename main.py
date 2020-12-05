a070f083b12b84ca054617ae077d5488ccdb0bb7
Dhadhazi <dhadhazi@gmail.com>
2020-12-05 09:54:29 +0100

    Part two complete

--- main.py
+++ main.py
@@ -19,12 +19,30 @@ def seat_decoder(seat_code):
     return {"column": column, "row": row}
 
 
+def get_seat_id(seat_code):
+    row = decoder(code=seat_code[:7], max=127)
+    column = decoder(code=seat_code[-3:], max=7)
+    return row * 8 + column
+
+
 highest_boarding_id = 0
 
+
 for line in data:
-    seat = seat_decoder(line)
-    seat_id = seat["row"] * 8 + seat["column"]
+    seat_id = get_seat_id(line)
     if seat_id > highest_boarding_id:
         highest_boarding_id = seat_id
 
 print(f"Highest boarding id: {highest_boarding_id}")
+
+#Part two
+all_seat_id = []
+for line in data:
+    all_seat_id.append(get_seat_id(line))
+
+all_seat_id.sort()
+
+for i in range(len(all_seat_id)):
+    if all_seat_id[i] + 1 != all_seat_id[i+1]:
+        print(f"Your seat ID is: {all_seat_id[i] + 1}")
+        break
